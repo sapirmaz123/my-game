4df11d8528a744ccab4413ccc01ff1252414b6e0
sapirmaz123 <sapirmaz123@gmail.com>
2022-02-08 10:03:45 +0200

    update

--- my_game.py
+++ my_game.py
@@ -13,6 +13,22 @@ pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
 
 
+colors_list= []
+def colors():
+    for i in range(WINDOW_W):
+        colors_list [i]= random_color()
+        return colors_list[i]
+    
+
+def circles():
+    y= 20
+    for j in range(4):
+        x= 20
+        for i in range(WINDOW_W):
+            pygame.draw.circle(screen, colors(), (x, y), 15)
+            x += 50
+        y += 50
+
 
 IMAGE= 'background.jpg'
 img= pygame.image.load(IMAGE)
@@ -22,21 +38,9 @@ def random_color():
     return tuple(random.choice(levels) for _ in range(3))
 
 
-screen.blit(img, (0,0))
 
-
-y= 20
-for j in range(4):
-    x= 20
-    for i in range(WINDOW_W):
-        pygame.draw.circle(screen, random_color(), (x, y), 15)
-        x += 50
-    y += 50
-
-
-x_step1= WINDOW_W/2-30
-x_step2= WINDOW_W/2+30
-
+x_start= WINDOW_W/2-30
+x_end= WINDOW_W/2+30
 
 play = True
 while play:
@@ -46,11 +50,16 @@ while play:
             play = False
         elif event.type == pygame.KEYDOWN: 
             if event.key == pygame.K_LEFT: 
-                x_step1 = -50
+                x_start -= 50
+                x_end -= 50
             if event.key == pygame.K_RIGHT: 
-                x_step1 += 50 
+                x_start += 50
+                x_end += 50 
+    
+    screen.blit(img, (0,0))
+    circles()
 
-    pygame.draw.line(screen, (0,200,0), [x_step1, 400], [x_step2, 400], width=10)
+    pygame.draw.line(screen, (0,200,0), [x_start, 400], [x_end, 400], width=10)
     pygame.display.flip()
     clock.tick(40)
 
